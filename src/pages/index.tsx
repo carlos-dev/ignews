23a198520388377f9c1d400d93ccfeeb0b952241
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2021-11-03 09:52:11 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/home.module.scss';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>início | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        hello
+
+        <span>world</span>
+      </h1>
+    </>
+  )
+}
